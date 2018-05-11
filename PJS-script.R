2c10966298f288301a96902aceda80086eeb7a1b
hkaspersen <hakon.kaspersen@protonmail.com>
2018-05-11 13:06:14 +0200

    worked on correctly spreading data

--- PJS-script.R
+++ PJS-script.R
@@ -41,13 +41,26 @@ artkoder_conv <- artkoder %>%
 hensikt <- sqlFetch(journal_rapp, sqtable = "hensikt")
 metode <- sqlFetch(journal_rapp, sqtable = "metode")
 
-metode_conv$metodekode <- rmv_wht(metode_conv$metodekode)
+metode$metodekode <- rmv_wht(metode$metodekode)
 
 metode_conv <- metode %>%
   select(metodekode, metodenavn) %>%
   mutate(metodekode = as.character(metodekode)) %>%
   filter(metodekode %in% c('70070','70231','70024','70152','30068','30020','60265','10002','10092','10057'))
 
+konklusjon <- sqlFetch(journal_rapp, sqtable = "konklusjonsregister")
+
+konklusjon_conv <- konklusjon %>%
+  mutate(konklusjonkode = as.character(konklusjonkode)) %>%
+  select(konklusjonkode, konklusjonnavn)
+
+provemateriale <- sqlFetch(journal_rapp, sqtable = "provemateriale")
+
+provemateriale_conv <- provemateriale %>%
+  mutate(provematerialekode = as.character(provematerialekode)) %>%
+  select(provematerialekode, provematerialenavn)
+
+resultat <- sqlFetch(journal_rapp, sqtable = "resultat")
 
 # Data queries
 myQuery <- "SELECT * 
@@ -69,27 +82,42 @@ rawdata_clean <- as.data.frame(lapply(rawdata,rmv_wht)) %>%
 filtered_data <- rawdata_clean %>%
   filter(konkl_analyttkode %in% analytt_conv$analyttkode) %>%
   mutate(artkode = as.character(gsub("^0(.*?)$","\\1", artkode)),
-         metodekode = as.character(gsub("^0(.*?)$", "\\1", metodekode))) %>%
+         metodekode = as.character(gsub("^0(.*?)$", "\\1", metodekode)),
+         konklusjonkode = as.character(gsub("^0(.*?)$", "\\1", konklusjonkode)),
+         provematerialekode = as.character(gsub("^0(.*?)$", "\\1", provematerialekode))) %>%
   filter(metodekode %in% metode_conv$metodekode) %>%
   left_join(., artkoder_conv, by = "artkode") %>%
   left_join(., metode_conv, by = "metodekode") %>%
+  left_join(., konklusjon_conv, by = "konklusjonkode") %>%
+  left_join(., provemateriale_conv, by = "provematerialekode") %>%
   mutate(analyttkode = case_when(analyttkode_funn == konkl_analyttkode ~ analyttkode_funn,
                                  TRUE ~ konkl_analyttkode)) %>%
   left_join(., analytt_conv, by = "analyttkode") %>%
   mutate(artnavn = factor(artnavn),
          analyttnavn = factor(analyttnavn),
          metodenavn = factor(metodenavn),
-         saksnr = paste(aar, ansvarlig_seksjon, innsendelsesnummer,provenummer, sep = "-")) %>%
-  select(aar,saksnr,provenummer,mottatt_dato,sak_avsluttet,
+         konklusjonnavn = factor(konklusjonnavn),
+         provematerialenavn = factor(provematerialenavn),
+         saksnr = paste(aar, ansvarlig_seksjon, innsendelsesnummer, sep = "-"),
+         mottatt_dato = as.Date(mottatt_dato, "%d.%m.%y"),
+         avsluttet_dato = as.Date(avsluttet_dato, "%d.%m.%y"),
+         sak_avsluttet = as.Date(sak_avsluttet, "%d.%m.%y"),
+         unik_id = paste(saksnr,provenummer,delprovenummer,undersokelsesnummer,resultatnummer,sep = "-")) %>%
+  select(unik_id, aar,saksnr,provenummer,delprovenummer,provematerialenavn,mottatt_dato,sak_avsluttet,
          avsluttet_dato,eier_lokalitetnr,eier_lokalitetstype,
          eierreferanse,provematerialekode,metodenavn,artnavn,analyttnavn,
-         resultatnummer,konklusjonsnummer) %>%
-  group_by(saksnr, provenummer) %>%
-  summarise_all(funs(func_paste))
+         resultatnummer,konklusjonnavn) %>%
+  group_by(metodenavn, konklusjonnavn,analyttnavn) %>%
+  mutate(ind = row_number()) %>%
+  spread(metodenavn,konklusjonnavn) %>%
+  ungroup() %>%
+  mutate(PD = )
 
 test <- reshape(filtered_data, timevar = "provenummer", idvar = "saksnr", direction = "wide")
 
 
+filtered_data$saksnr <- factor(filtered_data$saksnr)
+count(filtered_data, saksnr)
 
 
 
