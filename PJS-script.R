e60f312e48a2ef0782bd0ac1f62d3e7177af8025
hkaspersen <hakon.kaspersen@protonmail.com>
2018-05-16 15:15:06 +0200

    updated functions and cleaned script file. Added list over counties

--- PJS-script.R
+++ PJS-script.R
@@ -1,81 +1,51 @@
 library(tidyverse)
 library(RODBC)
+library(tibble)
 
-## Functions
-# Remove whitespace in column(s)
-rmv_wht <- function(column) gsub('\\s+', '', column)
-func_paste <- function(x) paste(unique(sum(x, na.rm = T)))
+## Import functions
+source("functions.R")
 
 ## SQL queries
 # Creates a connection to the journal_rapp
-journal_rapp<-odbcDriverConnect(paste("DRIVER=", "SQL Server",
-                                      ";Database=", "journal_rapp",
-                                      ";Server=", "sqlpjs",
-                                      ";Port=", "",
-                                      ";PROTOCOL=TCPIP",
-                                      ";UID=", scan("PJS_password.txt",what = character(), n = 1L, quiet = T),
-                                      ";PWD=", scan("PJS_password.txt",what = character(), n = 1L, skip = 1, quiet = T), sep = ""))
-
+journal_rapp <- odbcDriverConnect(
+  paste(
+    "DRIVER=",
+    "SQL Server",
+    ";Database=",
+    "journal_rapp",
+    ";Server=",
+    "sqlpjs",
+    ";Port=",
+    "",
+    ";PROTOCOL=TCPIP",
+    ";UID=",
+    scan(
+      "PJS_password.txt",
+      what = character(),
+      n = 1L,
+      quiet = T
+    ),
+    ";PWD=",
+    scan(
+      "PJS_password.txt",
+      what = character(),
+      n = 1L,
+      skip = 1,
+      quiet = T
+    ),
+    sep = ""
+  )
+)
 
 # Fetch available tables from connection
-available_tables <- sqlTables(journal_rapp)
 
-analyttkoder <- sqlFetch(journal_rapp, sqtable = "analytt")
+conv_tables <- fetch_sql_tables(journal_rapp)
+conv_tables_clean <- lapply(conv_tables, clean_sql_tables)
 
-analyttkoder$analyttkode <- rmv_wht(analyttkoder$analyttkode)
+list2env(conv_tables_clean, envir = .GlobalEnv)
 
-analytt_conv <- analyttkoder %>%
-  filter(analyttkode %in% c("1220104",
-                            "122010402",
-                            "122010403",
-                            "1502010235")) %>%
-  select(analyttkode, analyttnavn) %>%
-  mutate(analyttkode = paste0("0",analyttkode))
-
-artkoder <- sqlFetch(journal_rapp, sqtable = "art")
-artkoder$artkode <- rmv_wht(artkoder$artkode)
-artkoder_conv <- artkoder %>%
-  select(artkode, artnavn) %>%
-  mutate(artkode = as.character(artkode))
-
-hensikt <- sqlFetch(journal_rapp, sqtable = "hensikt")
-metode <- sqlFetch(journal_rapp, sqtable = "metode")
-
-metode$metodekode <- rmv_wht(metode$metodekode)
-
-metode_conv <- metode %>%
-  select(metodekode, metodenavn) %>%
-  mutate(metodekode = as.character(metodekode)) %>%
-  filter(metodekode %in% c('70070','70231','70024','70152','30068','30020','60265','10002','10092','10057')) %>%
-  mutate(metodenavn_kort = case_when(grepl("immun", metodenavn, ignore.case = T) == TRUE ~ "immunhistochem",
-                                     grepl("cellekultur", metodenavn, ignore.case = T) == TRUE ~ "cell_culture",
-                                     grepl("antistoffer", metodenavn, ignore.case = T) == TRUE ~ "neutralization_AB",
-                                     grepl("RT-PCR med sekvensering", metodenavn, ignore.case = T) == TRUE ~ "RT_PCR_Seq",
-                                     grepl("påvisning med real-time RT-PCR", metodenavn, ignore.case = T) == TRUE ~ "RT_PCR",
-                                     grepl("Virusanalyser med PCR", metodenavn, ignore.case = T) == TRUE ~ "PCR",
-                                     grepl("sekvensering for karakterisering", metodenavn, ignore.case = T) == TRUE ~ "Seq",
-                                     grepl("Histopatologi", metodenavn, ignore.case = T) == TRUE ~ "hist"))
-
-
-konklusjon <- sqlFetch(journal_rapp, sqtable = "konklusjonsregister")
-
-konklusjon_conv <- konklusjon %>%
-  mutate(konklusjonkode = as.character(konklusjonkode)) %>%
-  select(konklusjonkode, konklusjonnavn)
-
-provemateriale <- sqlFetch(journal_rapp, sqtable = "provemateriale")
-
-provemateriale_conv <- provemateriale %>%
-  mutate(provematerialekode = as.character(provematerialekode)) %>%
-  select(provematerialekode, provematerialenavn)
-
-resultat <- sqlFetch(journal_rapp, sqtable = "resultat")
-
-oppstalling <- sqlFetch(journal_rapp, sqtable = "oppstalling")
-
-oppstalling_conv <- oppstalling %>%
-  mutate(oppstallingkode = as.character(oppstallingkode)) %>%
-  select(oppstallingkode, oppstallingnavn)
+analyttkoder <- filter_analytes(analyttkoder)
+metode <- filter_methods(metode)
 
 
 # Data queries
@@ -90,59 +60,12 @@ rawdata <- sqlQuery(journal_rapp, query = myQuery, as.is = TRUE)
 # Close connection
 odbcCloseAll()
 
+# Import data on municipality and counties
+municipality <- import_municipality()
+counties <- import_counties()
+
 # Data wrangling
+rawdata_clean <- remove_whitespace_data(rawdata)
 
-rawdata_clean <- as.data.frame(lapply(rawdata,rmv_wht)) %>%
-  mutate_all(as.character)
+filtered_data <- data_wrangle(rawdata_clean)
 
-filtered_data <- rawdata_clean %>%
-  filter(konkl_analyttkode %in% analytt_conv$analyttkode) %>%
-  mutate(artkode = as.character(gsub("^0(.*?)$","\\1", artkode)),
-         metodekode = as.character(gsub("^0(.*?)$", "\\1", metodekode)),
-         konklusjonkode = as.character(gsub("^0(.*?)$", "\\1", konklusjonkode)),
-         provematerialekode = as.character(gsub("^0(.*?)$", "\\1", provematerialekode)),
-         oppstallingkode = as.character(gsub("^0(.*?)$", "\\1", oppstallingkode))) %>%
-  filter(metodekode %in% metode_conv$metodekode) %>%
-  left_join(., artkoder_conv, by = "artkode") %>%
-  left_join(., metode_conv, by = "metodekode") %>%
-  left_join(., konklusjon_conv, by = "konklusjonkode") %>%
-  left_join(., provemateriale_conv, by = "provematerialekode") %>%
-  left_join(., oppstalling_conv, by = "oppstallingkode") %>%
-  mutate(analyttkode = case_when(analyttkode_funn == konkl_analyttkode ~ analyttkode_funn,
-                                 TRUE ~ konkl_analyttkode)) %>%
-  left_join(., analytt_conv, by = "analyttkode") %>%
-  mutate(artnavn = factor(artnavn),
-         analyttnavn = factor(analyttnavn),
-         metodenavn = factor(metodenavn),
-         konklusjonnavn = factor(konklusjonnavn),
-         provematerialenavn = factor(provematerialenavn),
-         saksnr = paste(aar, ansvarlig_seksjon, innsendelsesnummer, sep = "-"),
-         mottatt_dato = as.Date(mottatt_dato, "%d.%m.%y"),
-         avsluttet_dato = as.Date(avsluttet_dato, "%d.%m.%y"),
-         sak_avsluttet = as.Date(sak_avsluttet, "%d.%m.%y"),
-         unik_id = paste(saksnr,provenummer,delprovenummer,undersokelsesnummer,resultatnummer,sep = "-")) %>%
-  select(unik_id, aar,saksnr,provenummer,delprovenummer,provematerialenavn,mottatt_dato,sak_avsluttet,
-         avsluttet_dato,eier_lokalitetnr,eier_lokalitetstype,oppstallingnavn,
-         eierreferanse,provematerialekode,metodenavn_kort,artnavn,analyttnavn,
-         resultatnummer,konklusjonnavn,kommunenr) %>%
-  group_by(metodenavn_kort, konklusjonnavn,analyttnavn) %>%
-  mutate(ind = row_number()) %>%
-  spread(metodenavn_kort,konklusjonnavn) %>%
-  group_by(unik_id) %>%
-  mutate(analysis_count = 1:n(),
-         singleton = n() == 1) %>%
-  ungroup() %>%
-  filter(singleton|analysis_count == 2) %>%
-  select(-analysis_count, -singleton, -ind) %>%
-  mutate(PD = case_when(cell_culture == "Påvist" ~ 1,
-                        hist == "Påvist" ~ 1,
-                        immunhistochem == "Påvist" ~ 1,
-                        RT_PCR == "Påvist" ~ 1,
-                        Seq == "Påvist" ~ 1,
-                        TRUE ~ 0)) %>%
-  group_by(PD,artnavn,oppstallingnavn,kommunenr) %>%
-  count() %>%
-  spread(PD, n, fill = 0) %>%
-  mutate(total = `0`+`1`,
-         pPD = `1`/total*100)
-  
--- functions.R
+++ functions.R
@@ -1,3 +1,10 @@
+# Remove whitespace in column(s)
+rmv_wht <- function(column) gsub('\\s+', '', column)
+
+# Paste together unique values from columns
+func_paste <- function(x) paste(unique(x))
+
+# Fetch sql tables from connection
 fetch_sql_tables <- function(connection) {
   analyttkoder <- sqlFetch(connection, sqtable = "analytt")
   artkoder <- sqlFetch(connection, sqtable = "art")
@@ -7,18 +14,20 @@ fetch_sql_tables <- function(connection) {
     sqlFetch(connection, sqtable = "konklusjonsregister")
   provemateriale <- sqlFetch(connection, sqtable = "provemateriale")
   oppstalling <- sqlFetch(connection, sqtable = "oppstalling")
+  driftsform <- sqlFetch(connection, sqtable = "driftsform")
   table_list <-
-    list(analyttkoder,
+    lst(analyttkoder,
          artkoder,
          hensikt,
          metode,
          konklusjon,
          provemateriale,
-         oppstalling)
-  names(table_list) <- c()
+         oppstalling,
+        driftsform)
   return(table_list)
 }
 
+# Clean sql tables
 clean_sql_tables <- function(df) {
   code_column <- grep("kode$", colnames(df), value = TRUE)
   key <- gsub("(.*?)kode$", "\\1", code_column)
@@ -31,23 +40,7 @@ clean_sql_tables <- function(df) {
   return(df)
 }
 
-import_counties <- function() {
-  counties <-
-    read.csv(
-      "https://register.geonorge.no/subregister/sosi-kodelister/kartverket/kommunenummer.csv?",
-      sep = ";",
-      encoding = "UTF-8"
-    )
-  
-  counties_conv <- counties %>%
-    mutate(Kodeverdi = sprintf("%04d", Kodeverdi)) %>%
-    rename(kommunenr = Kodeverdi,
-           kommunenavn = X.U.FEFF.Navn) %>%
-    select(kommunenr, kommunenavn, Status, Oppdatert) %>%
-    filter(Status == "Gyldig")
-  return(counties_conv)
-}
-
+# Fix codes in "kode" column
 fix_codes <- function(df) {
   code_column <- grep("kode$", colnames(df), value = TRUE)
   
@@ -56,6 +49,7 @@ fix_codes <- function(df) {
   return(df)
 }
 
+# Filter correct analytes
 filter_analytes <- function(df) {
   df <- df %>%
     filter(analyttkode %in% c("1220104",
@@ -65,12 +59,14 @@ filter_analytes <- function(df) {
   return(df)
 }
 
+# Filter correct methods
 filter_methods <- function(df) {
-  method_df <- method_df %>%
+  df <- df %>%
     mutate(
       metodenavn_kort = case_when(
         grepl("immun", metodenavn, ignore.case = T) == TRUE ~ "immunhistochem",
-        grepl("cellekultur", metodenavn, ignore.case = T) == TRUE ~ "cell_culture",
+        grepl("cellekultur", metodenavn, ignore.case = T) == TRUE |
+          grepl("Bakterier og sopp", metodenavn, ignore.case = T) == TRUE ~ "cell_culture",
         grepl("antistoffer", metodenavn, ignore.case = T) == TRUE ~ "neutralization_AB",
         grepl("RT-PCR med sekvensering", metodenavn, ignore.case = T) == TRUE ~ "RT_PCR_Seq",
         grepl("påvisning med real-time RT-PCR", metodenavn, ignore.case = T) == TRUE ~ "RT_PCR",
@@ -81,9 +77,135 @@ filter_methods <- function(df) {
     )
 }
 
+# Import municipality data from kartverket
+import_municipality <- function() {
+  municipality <-
+    read.csv(
+      "https://register.geonorge.no/subregister/sosi-kodelister/kartverket/kommunenummer.csv?",
+      sep = ";",
+      encoding = "UTF-8"
+    )
+  
+  municipality <- municipality %>%
+    mutate(Kodeverdi = sprintf("%04d", Kodeverdi)) %>%
+    rename(kommunenr = Kodeverdi,
+           kommunenavn = X.U.FEFF.Navn) %>%
+    select(kommunenr, kommunenavn, Status, Oppdatert) %>%
+    filter(Status == "Gyldig")
+  return(municipality)
+}
 
+# Import county data from table
+import_counties <- function() {
+  counties <- read.table("fylkeliste.txt", sep = "\t", stringsAsFactors = F, header = TRUE) %>%
+    select(Fylkesnummer,Fylker..18.stk.) %>%
+    rename(fylkenr = Fylkesnummer,
+           fylkenavn = Fylker..18.stk.) %>%
+    group_by(fylkenr) %>%
+    summarise_all(funs(func_paste)) %>%
+    mutate(fylkenr = gsub("Nr. (.*?)", "\\1", fylkenr),
+           fylkenr = rmv_wht(fylkenr))
+  return(counties)
+}
 
+# Removes whitespace from data frame and converts all columns to character
+remove_whitespace_data <- function(df) {
+  df <- as.data.frame(lapply(df, rmv_wht)) %>%
+    mutate_all(as.character)
+  return(df)
+}
 
-
-
-
+# Wrangle data frame to report
+data_wrangle <- function(df) {
+  df <- df %>%
+    filter(konkl_analyttkode %in% analyttkoder$analyttkode) %>%
+    mutate(
+      artkode = as.character(gsub("^0(.*?)$", "\\1", artkode)),
+      metodekode = as.character(gsub("^0(.*?)$", "\\1", metodekode)),
+      konklusjonkode = as.character(gsub("^0(.*?)$", "\\1", konklusjonkode)),
+      provematerialekode = as.character(gsub("^0(.*?)$", "\\1", provematerialekode)),
+      oppstallingkode = as.character(gsub("^0(.*?)$", "\\1", oppstallingkode)),
+      hensiktkode = as.character(gsub("^0(.*?)$", "\\1", hensiktkode)),
+      fylkenr = gsub("^(.*?)[0-9][0-9]$", "\\1", kommunenr)
+    ) %>%
+    filter(metodekode %in% metode$metodekode) %>%
+    left_join(., artkoder, by = "artkode") %>%
+    left_join(., metode, by = "metodekode") %>%
+    left_join(., konklusjon, by = "konklusjonkode") %>%
+    left_join(., provemateriale, by = "provematerialekode") %>%
+    left_join(., oppstalling, by = "oppstallingkode") %>%
+    left_join(., subset(municipality, select = c(kommunenr, kommunenavn)), by = "kommunenr") %>%
+    left_join(., driftsform, by = "driftsformkode") %>%
+    left_join(., counties, by = "fylkenr") %>%
+    left_join(., hensikt, by = "hensiktkode") %>%
+    mutate(
+      analyttkode = case_when(
+        analyttkode_funn == konkl_analyttkode ~ analyttkode_funn,
+        TRUE ~ konkl_analyttkode
+      )
+    ) %>%
+    left_join(., analyttkoder, by = "analyttkode") %>%
+    mutate(
+      artnavn = factor(artnavn),
+      analyttnavn = factor(analyttnavn),
+      metodenavn = factor(metodenavn),
+      konklusjonnavn = factor(konklusjonnavn),
+      provematerialenavn = factor(provematerialenavn),
+      saksnr = paste(aar, ansvarlig_seksjon, innsendelsesnummer, sep = "-"),
+      mottatt_dato = as.Date(mottatt_dato, "%d.%m.%y"),
+      avsluttet_dato = as.Date(avsluttet_dato, "%d.%m.%y"),
+      sak_avsluttet = as.Date(sak_avsluttet, "%d.%m.%y"),
+      unik_id = paste(
+        saksnr,
+        provenummer,
+        delprovenummer,
+        undersokelsesnummer,
+        resultatnummer,
+        sep = "-"
+      )
+    ) %>%
+    select(
+      unik_id,
+      aar,
+      mottatt_dato,
+      avsluttet_dato,
+      saksnr,
+      eier_lokalitetnr,
+      NAVN,
+      fylkenr,
+      fylkenavn,
+      kommunenavn,
+      hensiktnavn,
+      provematerialenavn,
+      metodenavn,
+      metodenavn_kort,
+      artnavn,
+      oppstallingnavn,
+      driftsformnavn,
+      analyttnavn,
+      resultatnummer,
+      konklusjonnavn
+    ) %>%
+    group_by(unik_id) %>%
+    mutate(
+      analysis_count = 1:n(),
+      singleton = n() == 1,
+      ind = row_number()
+    ) %>%
+    ungroup() %>%
+    filter(singleton | analysis_count == 2) %>%
+    select(-analysis_count,-singleton,-ind) %>%
+    group_by(metodenavn_kort, konklusjonnavn, analyttnavn) %>%
+    spread(metodenavn_kort, konklusjonnavn) %>%
+    mutate(
+      PD = case_when(
+        cell_culture == "Påvist" ~ 1,
+        hist == "Påvist" ~ 1,
+        immunhistochem == "Påvist" ~ 1,
+        RT_PCR == "Påvist" ~ 1,
+        Seq == "Påvist" ~ 1,
+        TRUE ~ 0
+      )
+    )
+  return(df)
+}
