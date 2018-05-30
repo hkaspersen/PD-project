8b248e46b9f2a80f43125d44c144a31476b1874b
hkaspersen <hakon.kaspersen@protonmail.com>
2018-05-30 15:51:23 +0200

    startup app for reporting

---
+++ PD_Rapportering/app.R
@@ -0,0 +1,56 @@
+library(shiny)
+
+## -------------------------------------- UI ------------------------------------------
+
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("Pancreas Disease in Norwegian Fish"),
+   
+   sidebarLayout(
+      sidebarPanel(
+         sliderInput("bins",
+                     "Number of bins:",
+                     min = 1,
+                     max = 50,
+                     value = 30)
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+         plotOutput("distPlot")
+      )
+   )
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## ----------------------------------- Server -------------------------------------------
+server <- function(input, output) {
+   
+   output$distPlot <- renderPlot({
+      # generate bins based on input$bins from ui.R
+      x    <- faithful[, 2] 
+      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+      
+      # draw the histogram with the specified number of bins
+      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+   })
+}
+
+# Run application
+shinyApp(ui = ui, server = server)
+
