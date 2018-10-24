c820603c527a345c0685e24cef69e9f99b6cb786
hkaspersen <hakon.kaspersen@protonmail.com>
2018-10-24 12:28:32 +0200

    updated with new functions

--- get_data.R
+++ get_data.R
@@ -35,6 +35,10 @@ conv_tables <- fetch_sql_tables(journal_rapp) %>%
   lapply(., clean_sql_tables) %>%
   filter_methods(.)
 
+conv_tables$kjennelse_res <- conv_tables$konklusjon %>%
+  rename("resultatnavn" = konklusjonnavn,
+         "kjennelse_resultat" = konklusjonkode)
+
 analyttkoder <- sqlFetch(journal_rapp, sqtable = "analytt") %>%
   clean_sql_tables(.)
 
@@ -42,28 +46,29 @@ analyttkoder <- sqlFetch(journal_rapp, sqtable = "analytt") %>%
 # Data queries
 myQuery <- "SELECT * 
 FROM V1_SAK_M_RES_EIER 
-WHERE aar = 2018 AND
-(hensiktkode LIKE '02001%' OR 
-hensiktkode LIKE '07%' OR 
-hensiktkode LIKE '01001%' OR 
-hensiktkode LIKE '08001%')
-OR (artkode LIKE '01%' OR 
+WHERE 
+aar = 2018 AND
+(artkode LIKE '01%' OR 
 artkode LIKE '02%' OR 
 artkode LIKE '04%')
-OR (metodekode = '070070' OR
+AND(
+(hensiktkode = '0100109003' OR 
+hensiktkode = '0100108018' OR 
+hensiktkode = '0200150' OR 
+hensiktkode = '0100111003' OR
+hensiktkode = '0800109' OR
+hensiktkode = '08001')
+OR(metodekode = '070070' OR
 metodekode = '070231' OR
 metodekode = '070024' OR
 metodekode = '060265' OR
 metodekode = '010092' OR
 metodekode = '010057')
-OR (analyttkode_funn = '1220104' OR
-analyttkode_funn = '0122010402' OR
-analyttkode_funn = '0122010403' OR
+OR(analyttkode_funn LIKE '01220104%' OR
 analyttkode_funn = '1502010235')
-OR (konkl_analyttkode = '1220104' OR
-konkl_analyttkode = '0122010402' OR
-konkl_analyttkode = '0122010403' OR
-konkl_analyttkode = '1502010235')"
+OR(konkl_analyttkode LIKE '01220104%' OR
+konkl_analyttkode = '1502010235')
+)"
 
 rawdata <- sqlQuery(journal_rapp, query = myQuery, as.is = TRUE)
 
@@ -71,26 +76,9 @@ rawdata <- sqlQuery(journal_rapp, query = myQuery, as.is = TRUE)
 odbcCloseAll()
 
 # Data wrangling
-
-rawdata_clean <- rawdata %>%
-  remove_whitespace_data(.) %>%
-  mutate(saksnr = paste(aar, ansvarlig_seksjon, innsendelsesnummer, sep = "-"),
-         unik_id = paste(
-           saksnr,
-           provenummer,
-           delprovenummer,
-           undersokelsesnummer,
-           resultatnummer,
-           sep = "-"
-         ))
-
-data_list <- split(rawdata_clean, rawdata_clean$unik_id)
+rawdata_filtered <- filter_and_create_no(rawdata)
+data_list <- split(rawdata_filtered, rawdata_filtered$provenr)
 filtered_data_list <- lapply(data_list, function(x) select_analytes(x))
 filtered_data <- do.call(rbind, filtered_data_list)
-
-final_report <- create_report(filtered_data)
-
-stats_df <- final_report %>%
-  filter(!is.na(konklusjonnavn)) %>%
-  mutate(month_number = as.character(format(mottatt_dato, "%m"))) %>%
-  left_join(., month_names, by = "month_number")
+report <- create_report(filtered_data)
+report_filtered <- fix_report(report)
